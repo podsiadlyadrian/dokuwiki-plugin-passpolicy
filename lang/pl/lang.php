eb170490b90e641afc14e0df22d721f5fdf7d1bc
podsiadlyadrian <adrian.podsiadly@rid.pl>
2025-01-17 09:15:26 +0100

    Create translate lang.php in Polish

---
+++ lang/pl/lang.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * @author Adrian Podsiadły <adrian.podsiadly@rid.pl>
+ */
+
+$lang['badpasspolicy'] = 'Musisz użyć silniejszego hasła.';
+$lang['length'] = 'Twoje hasło musi mieć co najmniej %d znaków.';
+$lang['pools'] = 'Twoje hasło musi zawierać co najmniej %d znaków z następujących typów: %s.';
+$lang['user1'] = 'Twoje hasło nie może zawierać nazwy użytkownika.';
+$lang['user2'] = 'Hasło może składać się maksymalnie z %d kolejnych znaków występujących w nazwie użytkownika.';
+$lang['nocommon'] = 'Twoje hasło nie może znajdować się wśród 10 000 najczęściej używanych haseł.';
+$lang['noleaked'] = 'Twoje hasło nie może zostać wykorzystane zgodnie w żadnym znanym wycieku haseł.';
+$lang['resendpwd'] = 'Jeśli konto istnieje, do podanego użytkownika wysłano wiadomość e-mail z potwierdzeniem.';
+
+$lang['js']['strength0'] = 'bardzo słabe';
+$lang['js']['strength1'] = 'słabe';
+$lang['js']['strength2'] = 'skromne';
+$lang['js']['strength3'] = 'silne';
+$lang['js']['nopolicy'] = ', polityka nie spełniona';
