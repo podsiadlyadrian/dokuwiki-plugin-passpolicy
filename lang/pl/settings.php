baf878f448cb31f415bdfcbe4cc83bd5430082ed
podsiadlyadrian <adrian.podsiadly@rid.pl>
2025-01-17 09:20:35 +0100

    Create settings.php translate to Polish

---
+++ lang/pl/settings.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Adrian Podsiadły <adrian.podsiadly@rid.pl>
+ */
+$lang['minlen'] = 'Minimalna długość haseł użytkowników';
+$lang['pools'] = 'Znaki odpowiednie do haseł';
+$lang['minpools'] = 'Minimalna liczba różnych znaków, które muszą się znaleźć w haśle, nie może być większa od liczby znaków wybranych powyżej.';
+$lang['user'] = 'Sprawdź czy hasło jest zgodne z nazwą użytkownika. 0 wyłącza test, 1 odpowiada dokładnemu dopasowaniu. Każda inna liczba odpowiada liczbie kolejnych znaków, które mogą występować w nazwie użytkownika i haśle.';
+$lang['nocommon'] = 'Porównaj Hasło z listą 10 000 najpopularniejszych haseł.';
+$lang['autotype'] = 'Jak powinny być generowane hasła?';
+$lang['autobits'] = 'Minimalna liczba bitów informacji potrzebnych do wygenerowania hasła. Im wyższa liczba, tym hasło jest bezpieczniejsze, ale jednocześnie trudniej je zapamiętać. Minimalna: 24';
+$lang['autotype_o_random'] = 'Losowe hasło';
+$lang['autotype_o_pronouncable'] = 'Hasło możliwe do użycia';
+$lang['autotype_o_phrase'] = 'Fraza hasła składająca się z kilku słów';
+$lang['pools_lower'] = 'Małe litery';
+$lang['pools_upper'] = 'Wielkie litery';
+$lang['pools_numeric'] = 'Liczby';
+$lang['pools_special'] = 'Znaki specjalne (np. !, $, #, %)';
